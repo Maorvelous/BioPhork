b3978fc08f75b75384e3ce6f43e479cd02350995
Maorvelous <mao5000@hotmail.it>
2015-11-15 20:47:48 +0100

    15-11-2015 - Task 0.0.0-4 - url.py blocked

--- wsgi/biophork/biophork_app/urls.py
+++ wsgi/biophork/biophork_app/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls import include, url
 
 urlpatterns = [
     #url(r'^admin/', include(admin.site.urls)),
-    #url(r'^(?P<pk>\d+)/$', biophork_app.dna_sequence.as_view(), name='detail'),
+    
 ]
