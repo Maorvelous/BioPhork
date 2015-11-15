451f0996d5da1c4b873cbda382921d067aa01e08
Maorvelous <mao5000@hotmail.it>
2015-11-15 23:02:40 +0100

    15-11-2015 - Task 0.0.0-10 - change import in url.py

--- wsgi/biophork/biophork/urls.py
+++ wsgi/biophork/biophork/urls.py
@@ -18,8 +18,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+import wsgi.biophork.biophork.views
+
+admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^(?P<pk>\d+)/$', biophork.biophork.dna_sequence.as_view(), name='detail'),
+    url(r'^(?P<pk>\d+)/$', wsgi.biophork.biophork.dna_sequence.as_view(), name='detail'),
 ]
