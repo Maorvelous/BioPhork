7405315a600f38542854d64ab71707db5e288f44
Maorvelous <mao5000@hotmail.it>
2015-11-15 20:06:43 +0100

    15-11-2015 - Task 0.0.0-3 - Index not working

--- wsgi/biophork/biophork_app/urls.py
+++ wsgi/biophork/biophork_app/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 
 from django.conf.urls import include, url
-from django.contrib import admin
+#from django.contrib import admin
 
 
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^(?P<pk>\d+)/$', biophork_app.dna_sequence.as_view(), name='detail'),
+    #url(r'^admin/', include(admin.site.urls)),
+    #url(r'^(?P<pk>\d+)/$', biophork_app.dna_sequence.as_view(), name='detail'),
 ]
