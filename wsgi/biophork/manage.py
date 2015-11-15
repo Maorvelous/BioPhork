471ff963b2638e9c018b6faabd666e1754a7e62c
Maorvelous <mao5000@hotmail.it>
2015-11-15 19:36:13 +0100

    15-11-2015 - Task 0.0.0-2 - Modifying manage.py

--- wsgi/biophork/manage.py
+++ wsgi/biophork/manage.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
     # GETTING-STARTED: change 'biophork' to your project name:
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "biophork.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "biophork_app.settings")
 
     from django.core.management import execute_from_command_line
 
